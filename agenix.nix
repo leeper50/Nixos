842f74f5749aa9c74f1d99c966c317189ebf9903
Walter Leeper <wleeper13@outlook.com>
2026-03-11 10:21:20 -0500

    switch off flake

--- agenix.nix
+++ agenix.nix
@@ -1,8 +1,22 @@
+# /etc/nixos/modules/agenix.nix
 { config, pkgs, ... }:
+
 let
-  secrets = import ./secrets/secrets.nix;  # adjust path as needed
+  # Download the agenix source once (to avoid repeated fetches)
+  agenixSrc = builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz";
+  secrets = import ./secrets/secrets.nix;
 in
 {
+  # import the library
+  imports = [
+    "${agenixSrc}/modules/age.nix"
+  ];
+
+  # install the command line utility
+  environment.systemPackages = with pkgs; [
+    (callPackage "${agenixSrc}/pkgs/agenix.nix" {})
+  ];
+
   # pull all secrets into the config
   age.secrets = builtins.mapAttrs (name: attrs: {
     file = ./secrets/${name};   # adjust path based on your module location
--- flake.nix
+++ flake.nix
@@ -2,10 +2,6 @@
   description = "Home Manager configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    agenix = {
-      url = "github:yaxitech/ragenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +9,6 @@
   };
   outputs =
     inputs@{
-      agenix,
       home-manager,
       nixpkgs,
       ...
@@ -23,7 +18,6 @@
         server = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            agenix.nixosModules.default
             ./agenix.nix
             ./configuration.nix
             ./podman.nix
