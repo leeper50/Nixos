aac88bee4ca2add5878801a67d264691e1a1ca9c
Walter Leeper <wleeper13@outlook.com>
2026-03-11 10:36:06 -0500

    no agenix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Home Manager configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    agenix = {
-      url = "github:yaxitech/ragenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # agenix = {
+    # url = "github:yaxitech/ragenix";
+    # inputs.nixpkgs.follows = "nixpkgs";
+    # };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +13,7 @@
   };
   outputs =
     {
-      agenix,
+      # agenix,
       home-manager,
       nixpkgs,
       ...
@@ -23,12 +23,12 @@
         server = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            agenix.nixosModules.default
-            ./agenix.nix
+            # agenix.nixosModules.default
+            # ./agenix.nix
             ./configuration.nix
             ./podman.nix
             ./samba.nix
-            ./secrets/secrets.nix
+            # ./secrets/secrets.nix
             ./user-walter.nix
             home-manager.nixosModules.home-manager
             {
