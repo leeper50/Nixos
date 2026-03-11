eb3f666d1865bc255c422a2ff3cb01882a017a9a
Walter Leeper <wleeper13@outlook.com>
2026-03-11 10:04:25 -0500

    fix paths

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,6 +4,6 @@ let
   keys = [ email primary ];
 in
 {
-  "user-walter-clear.age".publicKeys = keys;
-  "user-walter-hash.age".publicKeys = keys;
+  "secrets/user-walter-clear.age".publicKeys = keys;
+  "secrets/user-walter-hash.age".publicKeys = keys;
 }
