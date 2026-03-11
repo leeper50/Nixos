4d447452efb1ae8e3bf30d6853f8b82f5bb389d3
Walter Leeper <wleeper13@outlook.com>
2026-03-11 10:17:50 -0500

    switch module

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         server = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            agenix.homeManagerModules.default
+            agenix.nixosModules.default
             ./agenix.nix
             ./configuration.nix
             ./podman.nix
