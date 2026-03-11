a35436973acef9542ae7305c91e7eccefab5d1bc
Walter Leeper <wleeper13@outlook.com>
2026-03-11 10:39:03 -0500

    fix shell definition

--- configuration.nix
+++ configuration.nix
@@ -89,7 +89,6 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxiUaRCIxik4Ptw9JUm/vJiUcKMxEPuGpdf5CZWGZ1Z Walter-PC"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbDcD3eXAYp+ra3OXFLEDABbvVcBpY5yHEv9JULMBdW wleeper13@outlook.com"
       ];
-      shell = pkgs.fish;
     };
   };
 
