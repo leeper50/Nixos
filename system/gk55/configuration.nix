ef30b1a219f825067abfbe5f93c57100aef2599a
Walter Leeper <wleeper13@outlook.com>
2026-03-12 22:42:40 -0500

    cleanup comments

--- system/gk55/configuration.nix
+++ system/gk55/configuration.nix
@@ -4,12 +4,10 @@
     ./hardware-configuration.nix
   ];
 
-  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Enable networking
   networking = {
     hostName = "gk55";
     interfaces.enp1s0 = {
@@ -22,7 +20,6 @@
     };
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     btrfs-progs
@@ -34,11 +31,5 @@
     samba
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 }
--- system/ser8/configuration.nix
+++ system/ser8/configuration.nix
@@ -4,13 +4,11 @@
     ./hardware-configuration.nix
   ];
 
-  # Bootloader
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Enable networking
   networking = {
     hostName = "ser8";
     interfaces.ens18 = {
@@ -23,7 +21,6 @@
     };
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     btrfs-progs
@@ -41,11 +38,5 @@
     fileSystems = [ "/mnt/data" ];
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 }
