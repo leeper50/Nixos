e7dbc0eb0f9e2246f83defb2f458923ab5b39437
Walter Leeper <wleeper13@outlook.com>
2026-03-19 15:48:14 -0500

    add allowed-origin

--- services/cockpit.nix
+++ services/cockpit.nix
@@ -1,8 +1,13 @@
 { ... }:
 {
   services.cockpit = {
+    allowed-origins = [
+      "https://gk55.local:9090"
+      "https://ser8.local:9090"
+    ];
     enable = true;
     openFirewall = true;
+    port = 9090;
     settings = {
       WebService = {
         AllowUnencrypted = true;
