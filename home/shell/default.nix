34e4946bfef26e3468fbd09748ee4235e217815f
Walter Leeper <wleeper13@outlook.com>
2026-03-31 23:41:23 -0500

    add xdg path

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -16,6 +16,7 @@
         set -gx TERM xterm-256color
         set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
         set -gx MANROFFOPT -c
+        set -gx XDG_CONFIG_DIRS $HOME/.nix-profile/etc/xdg /etc/xdg
         fish_add_path $HOME/.local/bin $HOME/.cargo/bin $HOME/.dotnet/tools $HOME/.bun/bin $HOME/go/bin
         if type -q nixos-rebuild
             set -gx FLAKE_DIR /etc/nixos
