4cba24e9d4c5a468d764bd57862f376cc617c86f
Walter Leeper <wleeper13@outlook.com>
2026-04-12 22:44:52 -0500

    add swarm key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   gk55 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGHhitPnvUzImWRb80A31LvBoQru3BXUMb+lgDUKkE0 root@nixos";
   ser8 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKF/+u9TUbzAla9ejzsvGGkrLCHTcQ2gN3UJaXKUDlns root@nixos";
+  swarm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEqdihzgtsfoSrS6YVx4LCkPuuTRWV5GWOPbmhWTAhrf root@swarm";
   personal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxiUaRCIxik4Ptw9JUm/vJiUcKMxEPuGpdf5CZWGZ1Z Walter-PC";
   keys = [
     gk55
