011ef50ca255460ebc4373bd8509e3079c90a0db
Walter Leeper <wleeper13@outlook.com>
2026-04-15 16:27:17 -0500

    add more cores

--- home/games/default.nix
+++ home/games/default.nix
@@ -1,19 +1,22 @@
 { lib, pkgs, ... }:
 let
-  retroarch_dir = "~/Sync/Desktops/Retroarch";
+  retroarch_dir = "~/Sync/Retroarch";
 in
 {
   home.packages =
     with pkgs;
     [
+      azahar
       mumble
     ]
     ++ lib.optionals pkgs.stdenv.isLinux [
       faugus-launcher
       protonplus
       protontricks
+      rpcs3
       teamspeak6-client
       wowup-cf
+      xenia-canary
     ]
     ++ lib.optionals pkgs.stdenv.isDarwin [
     ];
@@ -32,6 +35,8 @@ in
         bsnes-hd.enable = true;
         citra.enable = true;
         dolphin.enable = true;
+        flycast.enable = true;
+        genesis-plus-gx.enable = true;
         melonds.enable = true;
         mesen.enable = true;
         mgba.enable = true;
@@ -52,6 +57,7 @@ in
         content_database_path = "${retroarch_dir}/ContentDatabase";
         cheat_database_path = "${retroarch_dir}/Cheats";
         playlist_directory = "${retroarch_dir}/Playlists";
+        rgui_browser_directory = "${retroarch_dir}/Games";
         rgui_config_directory = "${retroarch_dir}/Config";
         savefile_directory = "${retroarch_dir}/Saves";
         savestate_directory = "${retroarch_dir}/States";
