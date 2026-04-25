b9db18c46ddbb9da1e9551d4a8f4bedeaf461486
Walter Leeper <wleeper13@outlook.com>
2026-04-25 12:21:20 -0500

    add nfs share to swarm

--- hosts/swarm/default.nix
+++ hosts/swarm/default.nix
@@ -25,5 +25,11 @@
     };
   };
 
+  fileSystems."/mnt/docker" = {
+    device = "nas.local:/docker";
+    fsType = "nfs";
+  };
+  boot.supportedFilesystems = [ "nfs" ];
+
   system.stateVersion = "25.11";
 }
