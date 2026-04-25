65ecf52b29ca9a40ead5817282a3fad5b0501bd0
Walter Leeper <wleeper13@outlook.com>
2026-04-25 11:56:43 -0500

    change nfs exports

--- nixos/services/nfs.nix
+++ nixos/services/nfs.nix
@@ -11,8 +11,8 @@
       createMountPoints = true;
       enable = true;
       exports = ''
-        /nfs          10.0.0.0/24(ro,fsid=0,no_subtree_check)
-        /nfs/docker   10.0.0.0/24(rw,sync,no_subtree_check,no_root_squash)
+        /export          10.0.0.0/24(ro,fsid=0,no_subtree_check)
+        /export/docker   10.0.0.0/24(rw,sync,no_subtree_check,no_root_squash)
       '';
     };
   };
