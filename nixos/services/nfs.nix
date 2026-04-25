21be561bbb9cce4b371c1bb394231be8c4e6531b
Walter Leeper <wleeper13@outlook.com>
2026-04-25 12:08:59 -0500

    add nfs3 ports

--- nixos/services/nfs.nix
+++ nixos/services/nfs.nix
@@ -13,8 +13,28 @@
         /export          10.0.0.0/24(ro,fsid=0,no_subtree_check)
         /export/docker   10.0.0.0/24(rw,sync,no_subtree_check,no_root_squash)
       '';
+      lockdPort = 4001;
+      mountdPort = 4002;
+      statdPort = 4000;
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 2049 ];
+  networking.firewall = {
+    allowedTCPPorts = [
+      111
+      2049
+      4000
+      4001
+      4002
+      20048
+    ];
+    allowedUDPPorts = [
+      111
+      2049
+      4000
+      4001
+      4002
+      20048
+    ];
+  };
 }
