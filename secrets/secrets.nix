1b5fc60140c75655e9123bf34360635a02c4a7a2
Walter Leeper <wleeper13@outlook.com>
2026-04-27 23:23:40 -0500

    rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,20 +5,20 @@ let
   node-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvE1NTqdE7/ZCv+U25coA5rFxIDVUarbCPt3+1J10Lo root@node-2";
   node-3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID6RmulzmQsxyfgJlX5leCdgcUCvoHnbbaOHoqOwYlfs root@node-3";
   personal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxiUaRCIxik4Ptw9JUm/vJiUcKMxEPuGpdf5CZWGZ1Z Walter-PC";
-  swarm_keys = [
+  k3_swarm_keys = [
     node-1
     node-2
     node-3
     personal
   ];
-  keys = [
+  all_keys = [
     gk55
     nas
   ]
-  ++ swarm_keys;
+  ++ k3_swarm_keys;
 in
 {
-  "k3s_token.age".publicKeys = swarm_keys;
-  "user_walter_clear.age".publicKeys = keys;
-  "user_walter_hash.age".publicKeys = keys;
+  "k3s_token.age".publicKeys = all_keys;
+  "user_walter_clear.age".publicKeys = all_keys;
+  "user_walter_hash.age".publicKeys = all_keys;
 }
