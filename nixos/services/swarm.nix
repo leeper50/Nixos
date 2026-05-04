c07685a599a4140c4ed379c4ccaac3b4629be228
Walter Leeper <wleeper13@outlook.com>
2026-05-04 18:08:35 -0500

    fix path for ipforward

--- nixos/services/swarm.nix
+++ nixos/services/swarm.nix
@@ -38,10 +38,9 @@
       apply_ipforward() {
         for netns in /run/docker/netns/*; do
           if [ -e "$netns" ]; then
-            nsname=$(basename "$netns")
-            nsenter --net="$netns" sysctl -w net.ipv4.ip_forward=1 2>/dev/null && \
-              echo "Applied ip_forward to $nsname" || \
-              echo "Failed to apply to $nsname"
+            nsenter --net="/run/docker/netns/$netns" sysctl -w net.ipv4.ip_forward=1 2>/dev/null && \
+              echo "Applied ip_forward to $netns" || \
+              echo "Failed to apply to $netns"
           fi
         done
       }
