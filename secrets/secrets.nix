090df81c0547851a0eb38e1cbb4590bc5861f63b
Walter Leeper <wleeper13@outlook.com>
2026-05-04 13:39:47 -0500

    rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,9 @@
 let
   gk55 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGHhitPnvUzImWRb80A31LvBoQru3BXUMb+lgDUKkE0 root@nixos";
   nas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISsnpyceiNgLPCVpZiCuZ06a9Zpl3kUKmCCqRI6RFn2 root@nas";
-  node-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFeYUBJfc4KaXEi4ZU+9iIhGo6d7Q26U0DaDeGlLFu6 root@node-1";
-  node-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHAXbx862PzPMJZAzGkl+bfghtbWCSONNJf3l+34HCbg root@node-2";
-  node-3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFWricdYPgzpDg/AEdjw4cUSCBM2LBOP3JexKsn3p41 root@node-3";
+  node-1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOWw3itt6X+guXpUY1m5M2inL0Zs+Fs0nTrUOqDwZ/c root@node-1";
+  node-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF99jYxJYq1frbpyemmxb7+G4+N0Q0XF77sNDiQcphc4 root@node-2";
+  node-3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqG4JLWJ+lFKdOqTnY/gNHMoYLx82NjaTmwE7Lo1tJG root@node-3";
   personal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxiUaRCIxik4Ptw9JUm/vJiUcKMxEPuGpdf5CZWGZ1Z Walter-PC";
   k3_swarm_keys = [
     node-1
