5c11707b5cb38091405804bd0b1a8a0bcc0a205e
Walter Leeper <wleeper13@outlook.com>
2026-05-04 22:15:22 -0500

    keep swarm_token off nas

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,7 +5,7 @@ let
   node-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF99jYxJYq1frbpyemmxb7+G4+N0Q0XF77sNDiQcphc4 root@node-2";
   node-3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqG4JLWJ+lFKdOqTnY/gNHMoYLx82NjaTmwE7Lo1tJG root@node-3";
   personal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxiUaRCIxik4Ptw9JUm/vJiUcKMxEPuGpdf5CZWGZ1Z Walter-PC";
-  k3_swarm_keys = [
+  swarm_keys = [
     node-1
     node-2
     node-3
@@ -15,10 +15,9 @@ let
     gk55
     nas
   ]
-  ++ k3_swarm_keys;
+  ++ swarm_keys;
 in
 {
-  "k3s_token.age".publicKeys = all_keys;
   "portainer_license.age" = {
     publicKeys = [
       node-1
@@ -26,7 +25,14 @@ in
     ];
     hosts = [ "node-1" ];
   };
-  "swarm_token.age".publicKeys = k3_swarm_keys;
+  "swarm_token.age" = {
+    publicKeys = swarm_keys;
+    hosts = [
+      "node-1"
+      "node-2"
+      "node-3"
+    ];
+  };
   "user_walter_clear.age".publicKeys = all_keys;
   "user_walter_hash.age".publicKeys = all_keys;
 }
