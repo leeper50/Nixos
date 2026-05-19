b23ee4294dcafeefcf6e47b5275875f57086ede7
Walter Leeper <wleeper13@outlook.com>
2026-05-19 12:11:49 -0500

    try environment file

--- nixos/services/murmur.nix
+++ nixos/services/murmur.nix
@@ -3,29 +3,13 @@
   services.murmur = {
     bandwidth = 192000;
     enable = true;
+    environmentFile = config.age.secrets."mumble_server_password.age".path;
     logToFile = true;
     openFirewall = true;
+    password = "$MURMUR_PASSWORD";
     registerHostname = "19280085.xyz";
     registerName = "DaBois";
     registerUrl = "https://19280085.xyz";
-    user = "walter";
     welcometext = "Merry Christmas!!";
-  };
-
-  # Set serverpassword using agenix
-  systemd.services.murmur = {
-    serviceConfig = {
-      ExecStartPre =
-        let
-          script = pkgs.writeShellScript "murmur-inject-password" ''
-            PASSWORD=$(cat ${config.age.secrets."mumble_server_password.age".path})
-            CONFIG=/home/walter/.murmurd/mumble-server.ini
-            # Remove any existing serverpassword line, then append the real one
-            sed -i '/^serverpassword=/d' "$CONFIG"
-            echo "serverpassword=$PASSWORD" >> "$CONFIG"
-          '';
-        in
-        [ "+${script}" ];
-    };
   };
 }
