abcb75e7e7f27596d29738b2d7ed8475392171f5
Walter Leeper <wleeper13@outlook.com>
2026-05-20 11:52:12 -0500

    attempt the ol ipv6 setup

--- hosts/servercheap/default.nix
+++ hosts/servercheap/default.nix
@@ -6,10 +6,19 @@
   ];
 
   networking = {
-    nameservers = [ "9.9.9.9" ];
+    nameservers = [
+      "9.9.9.9"
+      "149.112.112.112"
+      "2620:fe::fe"
+      "2620:fe::9"
+    ];
     hostName = "servercheap";
     defaultGateway = {
       address = "65.75.202.1";
+      interface = "ens3";
+    };
+    defaultGateway6 = {
+      address = "2606:cc0:11:2300::1";
       interface = "ens3";
     };
     interfaces.ens3 = {
@@ -19,9 +28,14 @@
           prefixLength = 25;
         }
       ];
+      ipv6.addresses = [
+        {
+          address = "2606:cc0:11:2351::1";
+          prefixLength = 64;
+        }
+      ];
     };
   };
 
-  services.openssh.enable = true;
   system.stateVersion = "25.11";
 }
