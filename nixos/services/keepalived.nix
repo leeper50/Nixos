5d5a1c5a8888db05f0c0cffdd66c2987dc2b1bae
Walter Leeper <wleeper13@outlook.com>
2026-06-24 13:34:55 -0500

    setup ipv6 keepalived

--- nixos/services/keepalived.nix
+++ nixos/services/keepalived.nix
@@ -6,39 +6,57 @@
 let
   nodes = {
     "node-1" = {
-      addr = "10.0.0.21";
+      ipv4Addr = "10.0.0.21";
+      ipv6Addr = "2600:1702:58c1:9acd::21";
       priority = 100;
     };
     "node-2" = {
-      addr = "10.0.0.22";
+      ipv4Addr = "10.0.0.22";
+      ipv6Addr = "2600:1702:58c1:9acd::22";
       priority = 90;
     };
     "node-3" = {
-      addr = "10.0.0.23";
+      ipv4Addr = "10.0.0.23";
+      ipv6Addr = "2600:1702:58c1:9acd::23";
       priority = 80;
     };
   };
   thisNode = nodes.${config.networking.hostName};
-  peerAddrs = lib.mapAttrsToList (_: v: v.addr) (
+  peeripv4Addrs = lib.mapAttrsToList (_: v: v.ipv4Addr) (
+    lib.filterAttrs (name: _: name != config.networking.hostName) nodes
+  );
+  peeripv6Addrs = lib.mapAttrsToList (_: v: v.ipv6Addr) (
     lib.filterAttrs (name: _: name != config.networking.hostName) nodes
   );
 in
 {
   services.keepalived = {
     enable = true;
-    vrrpInstances.swarm = {
+    vrrpInstances.swarmipv4 = {
       interface = "eth0";
-      state = if config.networking.hostName == "node-1" then "MASTER" else "BACKUP";
-      virtualRouterId = 51;
       priority = thisNode.priority;
+      state = if config.networking.hostName == "node-1" then "MASTER" else "BACKUP";
+      unicastPeers = peeripv4Addrs;
+      unicastSrcIp = thisNode.ipv4Addr;
       virtualIps = [
         { addr = "10.0.1.1/8"; }
       ];
-      unicastSrcIp = thisNode.addr;
-      unicastPeers = peerAddrs;
+      virtualRouterId = 51;
+    };
+    vrrpInstances.swarmipv6 = {
+      interface = "eth0";
+      priority = thisNode.priority;
+      state = if config.networking.hostName == "node-1" then "MASTER" else "BACKUP";
+      unicastSrcIp = thisNode.ipv6Addr;
+      unicastPeers = peeripv6Addrs;
+      virtualIps = [
+        { addr = "2600:1702:58c1:9acd::1:1/64"; }
+      ];
+      virtualRouterId = 52;
     };
   };
   networking.firewall.extraCommands = ''
     iptables -A nixos-fw -p 112 -j nixos-fw-accept
+    ip6tables -A nixos-fw -p 112 -j nixos-fw-accept
   '';
 }
