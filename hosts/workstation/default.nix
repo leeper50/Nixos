984fa48f66bf12b77855b50457dbca0137ee763c
Walter Leeper <wleeper13@outlook.com>
2026-07-09 07:11:48 -0500

    add more game paths

--- hosts/workstation/default.nix
+++ hosts/workstation/default.nix
@@ -36,6 +36,7 @@ in
     steam = {
       exclude = [ ];
       paths = [
+        "/home/walter/.config/unity3d/Ludeon Studios"
         "/home/walter/.local/share/Steam/steamapps/common/Europa Universalis IV/builtin_dlc"
         "/home/walter/.local/share/Steam/steamapps/common/Europa Universalis IV/dlc_metadata"
         "/home/walter/.local/share/Steam/steamapps/common/Europa Universalis IV/dlc"
@@ -43,6 +44,8 @@ in
         "/home/walter/.local/share/Steam/steamapps/common/Victoria 3/game/dlc_metadata"
         "/home/walter/.local/share/Steam/steamapps/common/Victoria 3/game/dlc"
         "/home/walter/.local/share/Steam/steamapps/workshop/content/529340/2883019620"
+        "/home/walter/.local/share/Paradox Interactive"
+        "/home/walter/.paradoxlauncher"
       ];
     };
   };
