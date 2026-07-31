bc35463fd9e619c69c44c8317d545b26354db137
Walter Leeper <wleeper@mailbox.org>
2026-07-31 14:21:04 -0500

    add dns check script to adguard

--- nixos/services/keepalived.nix
+++ nixos/services/keepalived.nix
@@ -1,24 +1,32 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
+  checkAdguard = pkgs.writeShellScript "check-adguard" ''
+    answer=$(${pkgs.dnsutils}/bin/dig +short +time=1 +tries=1 @127.0.0.1 dnstest.dellhplaptop.xyz A) || exit 1
+    [ -n "$answer" ]
+  '';
   nodes = {
     "node-1" = {
       ipv4Addr = "10.0.0.21";
       ipv6Addr = "2600:1702:58c1:9acf::21";
       priority = 100;
+      state = "MASTER";
     };
     "node-2" = {
       ipv4Addr = "10.0.0.22";
       ipv6Addr = "2600:1702:58c1:9acf::22";
       priority = 90;
+      state = "BACKUP";
     };
     "node-3" = {
       ipv4Addr = "10.0.0.23";
       ipv6Addr = "2600:1702:58c1:9acf::23";
       priority = 80;
+      state = "BACKUP";
     };
   };
   thisNode = nodes.${config.networking.hostName};
@@ -34,10 +42,20 @@ in
     {
       services.keepalived = {
         enable = true;
+        enableScriptSecurity = true;
+        extraConfig = ''
+          vrrp_sync_group dns {
+            group {
+              dnsIPv4
+              dnsIPv6
+            }
+          }
+        '';
         vrrpInstances.dnsIPv4 = {
           interface = "eth0";
           priority = thisNode.priority;
-          state = if config.networking.hostName == "node-1" then "MASTER" else "BACKUP";
+          state = thisNode.state;
+          trackScripts = [ "chkAdguard" ];
           unicastPeers = peeripv4Addrs;
           unicastSrcIp = thisNode.ipv4Addr;
           virtualIps = [
@@ -48,13 +66,28 @@ in
         vrrpInstances.dnsIPv6 = {
           interface = "eth0";
           priority = thisNode.priority;
-          state = if config.networking.hostName == "node-1" then "MASTER" else "BACKUP";
+          state = thisNode.state;
+          trackScripts = [ "chkAdguard" ];
           unicastSrcIp = thisNode.ipv6Addr;
           unicastPeers = peeripv6Addrs;
           virtualIps = [
             { addr = "2600:1702:58c1:9acf::1:1/64"; }
           ];
           virtualRouterId = 52;
+        };
+        vrrpScripts.chkAdguard = {
+          fall = 2;
+          interval = 2;
+          rise = 2;
+          script = "${checkAdguard}";
+          timeout = 2;
+        };
+      };
+      users = {
+        groups.keepalived_script = { };
+        users.keepalived_script = {
+          group = "keepalived_script";
+          isSystemUser = true;
         };
       };
     }
