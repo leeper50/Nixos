618cbeeda3a8fb44263e821f7514dbfd4bf58c63
Walter Leeper <wleeper@mailbox.org>
2026-08-09 12:40:58 -0500

    add searxng to firefox

--- home/firefox/search.nix
+++ home/firefox/search.nix
@@ -1,11 +1,17 @@
 {
-  default = "ddg_noai";
+  default = "searxng";
   engines = {
     ddg_noai = {
+      definedAliases = [ "@dd" ];
+      iconMapObj."16" = "https://noai.duckduckgo.com/favicon.ico";
       name = "Duckduckgo No AI";
       urls = [ { template = "https://noai.duckduckgo.com/?q={searchTerms}"; } ];
-      iconMapObj."16" = "https://noai.duckduckgo.com/favicon.ico";
-      definedAliases = [ "@dd" ];
+    };
+    searxng = {
+      definedAliases = [ "@sx" ];
+      iconMapObj."16" = "https://sx.dellhplaptop.xyz/favicon.ico";
+      name = "Searxng";
+      urls = [ { template = "https://sx.dellhplaptop.xyz/?q={searchTerms}"; } ];
     };
   };
   force = true;
