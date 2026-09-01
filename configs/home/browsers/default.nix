d5db8280d66a0107abb8ec1703ed8eda4c824369
Walter Leeper <wleeper@mailbox.org>
2026-09-01 10:14:58 -0500

    allow new domain through proxy

--- configs/home/browsers/default.nix
+++ configs/home/browsers/default.nix
@@ -36,7 +36,7 @@ let
           "general.autoScroll" = true;
           "image.jxl.enabled" = true;
           "network.proxy.autoconfig_url" = "https://c.dellhplaptop.xyz/public/proxy.pac";
-          "network.proxy.no_proxies_on" = "localhost,dellhplaptop.xyz,buncha.men,10.0.0.0/8";
+          "network.proxy.no_proxies_on" = "localhost,dellhp.party,dellhplaptop.xyz,buncha.men,10.0.0.0/8";
         };
       };
     };
