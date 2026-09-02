f04c96d3135ec9d584d7a0ce3a7690cd7811d66b
Walter Leeper <wleeper@mailbox.org>
2026-09-02 18:30:10 -0500

    switch beszel port name

--- configs/nixos/beszel.nix
+++ configs/nixos/beszel.nix
@@ -6,7 +6,7 @@
 }:
 let
   cfg = config.local.beszel;
-  ports.agent = 8090;
+  ports.hub = 8090;
 in
 {
   options.local.beszel = {
@@ -21,7 +21,7 @@ in
       services.beszel.agent = {
         enable = true;
         environment = {
-          "HUB_URL" = "http://${cfg.agent.hubHost}:${toString ports.agent}";
+          "HUB_URL" = "http://${cfg.agent.hubHost}:${toString ports.hub}";
           "KEY" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNcggS7ZMLtGUfM0HTrqfj9jK6ezXoSj1MzDJOv6cOs";
           "TOKEN_FILE" = config.age.secrets."beszel_token.age".path;
         };
@@ -29,10 +29,18 @@ in
       users.groups.beszel-agent.gid = 992;
     }
     (lib.mkIf cfg.hub.enable {
+      networking.firewall = globals.mkFirewallRules {
+        service = "beszel";
+        sources = [
+          globals.networking.ipv4.lanSubnet
+          globals.networking.ipv6.lanSubnet
+        ];
+        tcpPorts = [ ports.hub ];
+      };
       services.beszel.hub = {
         enable = true;
         host = "0.0.0.0";
-        port = ports.agent;
+        port = ports.hub;
       };
     })
   ];
