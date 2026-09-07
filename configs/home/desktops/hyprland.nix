6481290d6cc477caca43324157b805a3cf0e045f
Walter Leeper <wleeper@mailbox.org>
2026-09-07 13:40:04 -0500

    use rg instead of grep

--- configs/home/desktops/hyprland.nix
+++ configs/home/desktops/hyprland.nix
@@ -7,7 +7,7 @@
 let
   cycle-audio-output = pkgs.writeShellScript "cycle-audio-output" ''
     current=$(${pactl} get-default-sink)
-    sinks=$(${pactl} list sinks short | awk '{print $2}' | grep alsa_output)
+    sinks=$(${pactl} list sinks short | awk '{print $2}' | rg alsa_output)
     count=$(echo "$sinks" | wc -l)
 
     current_idx=0
