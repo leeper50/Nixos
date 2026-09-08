8041cd31a339878a2137f57fe5509dcba0d6f6bc
Walter Leeper <wleeper@mailbox.org>
2026-09-08 13:15:42 -0500

    remove unused search

--- configs/home/browsers/search.nix
+++ configs/home/browsers/search.nix
@@ -7,12 +7,6 @@
       name = "Duckduckgo No AI";
       urls = [ { template = "https://noai.duckduckgo.com/?q={searchTerms}"; } ];
     };
-    searxng = {
-      definedAliases = [ "@sx" ];
-      iconMapObj."16" = "https://sx.dellhplaptop.xyz/favicon.ico";
-      name = "Searxng";
-      urls = [ { template = "https://sx.dellhplaptop.xyz/?q={searchTerms}"; } ];
-    };
   };
   force = true;
 }
