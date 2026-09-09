ee5195afc4df50e0ad6392a36c7d09d22f3fb61b
Walter Leeper <wleeper@mailbox.org>
2026-09-09 00:10:10 -0500

    use dark mode

--- configs/home/browsers/default.nix
+++ configs/home/browsers/default.nix
@@ -36,6 +36,7 @@ let
           "extensions.update.enable" = false;
           "general.autoScroll" = true;
           "image.jxl.enabled" = true;
+          "layout.css.prefers-color-scheme.content-override" = 0;
           "network.proxy.autoconfig_url" = "https://c.dellhplaptop.xyz/public/proxy.pac";
           "network.proxy.no_proxies_on" =
             "localhost,buncha.men,dellhplaptop.xyz,${globals.domain},${globals.networking.ipv4.lanSubnet}";
